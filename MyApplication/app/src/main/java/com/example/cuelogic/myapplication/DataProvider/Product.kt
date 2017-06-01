7e5aef0c52797cbcaa7bb8b964296495863712a8
Cuelogic <cuelogic@Cuelogics-Mac-mini.local>
2017-06-01 19:46:27 +0530

    Solve conflict in Product file

--- MyApplication/app/src/main/java/com/example/cuelogic/myapplication/DataProvider/Product.kt
+++ MyApplication/app/src/main/java/com/example/cuelogic/myapplication/DataProvider/Product.kt
@@ -11,5 +11,6 @@ class Product {
     var productImg: String? = null
     var vendorName: String = ""
     var vendorAddress: String = ""
-    var productImgGallary: String = "XYZ"
+    var productImgGallary: String = "ABC"
+    
 }
